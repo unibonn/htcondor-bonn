03511df0f367403ffb424f15229d7d2ff169135a
Oliver Freyermuth <o.freyermuth@googlemail.com>
2019-08-19 13:21:54 +0200

    Add remaining files needed for POV-Ray movie generation.

---
+++ files/create_pov_movie.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /etc/profile
+
+SCENE=$1
+
+cd render_results_${SCENE}
+ffmpeg -r 10 -f image2 -i video%03d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p -threads 4 ${SCENE}.mp4
+mv ${SCENE}.mp4 ../
